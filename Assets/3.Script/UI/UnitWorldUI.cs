ae23293511d64377581aeb849edec8a7388e5bff
오중근 <148051315+PortugaCode@users.noreply.github.com>
2023-12-26 16:05:44 +0900

    [Feat] isEnemy

--- Assets/3.Script/UI/UnitWorldUI.cs
+++ Assets/3.Script/UI/UnitWorldUI.cs
@@ -11,10 +11,12 @@ public class UnitWorldUI : MonoBehaviour
     [SerializeField] private Unit unit;
     [SerializeField] private Image healthBar;
     [SerializeField] private HealthSystem healthSystem;
+    [SerializeField] private Image enemyIcon;
 
     private void Start()
     {
         Unit.OnAnyActionPointsChanged += Unit_OnAnyActionPointsChanged;
+        enemyIcon.enabled = unit.IsEnemy();
         unit.OnDamage += Unit_OnDamage;
         unit.OnDie += Unit_OnDamage;
         UpdateHealthBar();
